361b130bb8f27a2833b9cbc33419555ea9451232
BPplays <andymalbp@gmail.com>
2023-11-04 09:14:41 -0700

    made it auto chown to user

--- main.go
+++ main.go
@@ -7,18 +7,9 @@ import (
 	"os/exec"
 	"os/user"
 	"path/filepath"
-	"slices"
 	"strings"
 	"time"
 )
-
-var white_list_dirs []string
-
-var white_list_dirs_nfs []string
-
-
-var home_maker_temp_directory string
-var uid_directory string
 
 
 func get_user_from_id(id string) (user.User, error) {
@@ -45,75 +36,19 @@ func get_user_from_id(id string) (user.User, error) {
 }
 
 
+func chown_r (file string, username string, group string) {
+	cmd := exec.Command("chown", fmt.Sprintf("%s:%s", username, username), "-R", file)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-// isNFS returns true if the given folder is on an NFS mounted drive
-func isNFS(folder string) (bool, error) {
-	// get the filesystem type of the folder
-	fsType, err := exec.Command("stat", "-f", "-c", "%T", folder).Output()
+	err := cmd.Run()
 	if err != nil {
-		return false, err
-	}
-	// compare the filesystem type with "nfs"
-	return strings.TrimSpace(string(fsType)) == "nfs", nil
-}
-
-
-func mk_home_dir(dir string, uid int, gid int) (error) {
-	will_make_dir := false
-	base_dir := filepath.Join(dir, "..")
-
-	if slices.Contains(white_list_dirs, base_dir) {
-		will_make_dir = true
-	}
-
-	if slices.Contains(white_list_dirs_nfs, base_dir) {
-		nfs_status, err := isNFS(base_dir)
-		if err != nil {
-			return err
-		}
-
-		if nfs_status {
-			will_make_dir = true
-		} else {
-			will_make_dir = false
-		}
-	}
-
-
-
-	if will_make_dir {
-		os.Mkdir(dir, 0700)
-		os.Chown(dir, uid, gid)
-	}
-	return nil
-}
-
-
-
-func make_dirs() {
-	err := os.MkdirAll(home_maker_temp_directory, 0755)
-	if err != nil {
-		log.Fatal(err, " home_maker_temp_directory", " ", home_maker_temp_directory)
-	}
-
-
-	err = os.Chmod(home_maker_temp_directory, 0755)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = os.MkdirAll(uid_directory, 0777)
-	if err != nil {
-		log.Fatal(err, " uid_directory")
-	}
-
-
-	err = os.Chmod(uid_directory, 0777)
-	if err != nil {
-		log.Fatal(err)
+		log.Println("Error running chown for", file, ":", err)
+	} else {
+		log.Println("chowned", file)
 	}
 }
-
 
 
 
@@ -138,6 +73,8 @@ func main() {
 				if _, err := os.Stat(keytabFile); err == nil {
 					filename := file.Name()
 	
+					chown_r(dirPath, filename, filename)
+
 					cmd := exec.Command("sudo", "-u", filename, "kinit", fmt.Sprintf("%s@SUZUKO.ORG", filename), "-k", "-t", keytabFile)
 					cmd.Stdin = os.Stdin
 					cmd.Stdout = os.Stdout
@@ -148,7 +85,6 @@ func main() {
 						log.Println("Error running kinit for", filename, ":", err)
 					} else {
 						log.Println("Ran kinit for", filename)
-						// fmt.Println(time.Now(), ": Ran kinit for", filename)
 					}
 				}
 			}
